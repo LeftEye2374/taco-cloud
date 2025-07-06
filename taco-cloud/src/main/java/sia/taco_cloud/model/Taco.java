408ad2b2c84568f51e731233e6c4b339dcd7f51b
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-06 20:04:25 +0300

    ...

--- taco-cloud/src/main/java/sia/taco_cloud/model/Taco.java
+++ taco-cloud/src/main/java/sia/taco_cloud/model/Taco.java
@@ -1,11 +1,18 @@
 package sia.taco_cloud.model;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.util.List;
 
 @Data
 public class Taco {
+    @NonNull
+    @Size(min=5, message = "Name must by at least 5 characters long")
     private String name;
+
+    @NonNull
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
