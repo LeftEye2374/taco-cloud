af427f030c5cbd389facedfa54cd6d5cc3d4cb4d
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-11 10:50:43 +0500

    ...

--- taco-cloud/src/main/java/sia/taco_cloud/repository/JdbcIngredientRepository.java
+++ taco-cloud/src/main/java/sia/taco_cloud/repository/JdbcIngredientRepository.java
@@ -47,6 +47,11 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
     @Override
     public Ingredient save(Ingredient ingredient) {
-        return null;
+        jdbcTemplate.update(
+                "insert into Ingredient (id,name,type) values (?,?,?)",
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getType().toString());
+        return ingredient;
     }
 }
