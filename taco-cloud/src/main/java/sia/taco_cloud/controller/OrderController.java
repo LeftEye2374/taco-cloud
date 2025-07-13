4a34090a29853659ec348232c596ae449a1f3523
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-13 17:02:03 +0500

    ...

--- taco-cloud/src/main/java/sia/taco_cloud/controller/OrderController.java
+++ taco-cloud/src/main/java/sia/taco_cloud/controller/OrderController.java
@@ -1,7 +1,10 @@
 package sia.taco_cloud.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +19,16 @@ import sia.taco_cloud.model.TacoOrder;
 public class OrderController {
 
     @GetMapping("/current")
-    public String orderForm(){
+    public String orderForm(Model model) {
+        model.addAttribute("tacoOrder", new TacoOrder());
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus){
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus){
+        if (errors.hasErrors()){
+            return "design";
+        }
         log.info("Order submitted: {}",order);
         sessionStatus.setComplete();
 
--- taco-cloud/src/main/java/sia/taco_cloud/model/Taco.java
+++ taco-cloud/src/main/java/sia/taco_cloud/model/Taco.java
@@ -1,5 +1,7 @@
 package sia.taco_cloud.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -7,6 +9,11 @@ import java.util.List;
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+
+    @NotNull
+    @Size(min = 1, message = "Your must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
