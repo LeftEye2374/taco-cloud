942c2b9dc0acfed07943f186e90ab5e788d537e7
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-16 12:22:52 +0500

    ...

--- taco-cloud/src/main/java/sia/taco_cloud/model/Taco.java
+++ taco-cloud/src/main/java/sia/taco_cloud/model/Taco.java
@@ -1,19 +1,20 @@
 package sia.taco_cloud.model;
 
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class Taco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt = new Date();
@@ -22,7 +23,11 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    @NotNull
     @Size(min = 1, message = "Your must choose at least 1 ingredient")
+    @ManyToOne
     private List<Ingredient> ingredients;
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
 }
