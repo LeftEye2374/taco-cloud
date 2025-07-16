0aff0016fe5ad82765ffd663e2f6798264ec8e0e
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-16 12:31:26 +0500

    ...

--- taco-cloud/src/main/java/sia/taco_cloud/repository/OrderRepository.java
+++ taco-cloud/src/main/java/sia/taco_cloud/repository/OrderRepository.java
@@ -1,9 +1,24 @@
 package sia.taco_cloud.repository;
 
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import sia.taco_cloud.model.TacoOrder;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder,String> {
+
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    List<TacoOrder> findByDeliveryToAndDeliveryCityAllIgnoreCase(String deliveryTo, String deliveryCity);
+
+    List<TacoOrder> findByDeliveryCityOrderByDeliveryTo(String deliveryCity);
+
+    @Query("Order o where o.deliveryCity='Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
 
 }
