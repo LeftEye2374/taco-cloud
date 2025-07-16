9b3e70133b02b55d82d7790f581c5c852f7491bf
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-16 16:06:23 +0500

    ...

--- taco-cloud/src/main/java/sia/taco_cloud/model/taco/Taco.java
+++ taco-cloud/src/main/java/sia/taco_cloud/model/taco/Taco.java
@@ -4,39 +4,26 @@ package sia.taco_cloud.model.taco;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.data.cassandra.core.cql.Ordering;
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.Column;
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
-import org.springframework.data.cassandra.core.mapping.Table;
 import sia.taco_cloud.model.ingredient.Ingredient;
-import sia.taco_cloud.model.ingredient.IngredientUDT;
 
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 @Data
-@Table("tacos")
 public class Taco {
-
-
-    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
-    private UUID id;
 
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    @PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
     private Date createdAt = new Date();
 
     @Size(min = 1, message = "Your must choose at least 1 ingredient")
     @Column("ingredients")
-    private List<IngredientUDT> ingredients;
+    private List<Ingredient> ingredients;
 
     public void addIngredient(Ingredient ingredient) {
-        this.ingredients.add(TacoUDRUtils.toIngredientUDT(ingredient));
+        this.ingredients.add(ingredient);
     }
 }
