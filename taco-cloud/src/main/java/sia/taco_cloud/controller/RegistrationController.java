304bd0669f5bffc117433f172230e1145a8bb900
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-17 14:31:35 +0500

    ...

--- taco-cloud/src/main/java/sia/taco_cloud/controller/RegistrationController.java
+++ taco-cloud/src/main/java/sia/taco_cloud/controller/RegistrationController.java
@@ -1,9 +1,31 @@
 package sia.taco_cloud.controller;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sia.taco_cloud.repository.UserRepository;
 
 @Controller
 @RequestMapping("/register")
 public class RegistrationController {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping
+    public String registerForm(){
+        return "register";
+    }
+
+    @PostMapping
+    public String processRegistration(RegistrationForm form){
+
+    }
 }
---
+++ taco-cloud/src/main/java/sia/taco_cloud/securite/RegistrationForm.java
@@ -0,0 +1,23 @@
+package sia.taco_cloud.securite;
+
+import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import sia.taco_cloud.model.user.User;
+
+@Data
+public class RegistrationForm {
+
+    private String username;
+    private String password;
+    private String fullName;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+
+    public User toUser(PasswordEncoder passwordEncoder){
+        return new User(username, passwordEncoder.encode(password),
+                fullName,street,city,zip,phone);
+    }
+}
