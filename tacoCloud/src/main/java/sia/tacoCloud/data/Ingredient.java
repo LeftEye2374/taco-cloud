39224272afc3cb27aeb66d98cc327965d9a8bec8
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-22 16:36:03 +0500

    Design form

--- tacoCloud/src/main/java/sia/tacoCloud/data/Ingredient.java
+++ tacoCloud/src/main/java/sia/tacoCloud/data/Ingredient.java
@@ -8,7 +8,10 @@ public class Ingredient {
     private String id;
     private String name;
     private Type type;
-    
+
+    public Ingredient(String sotName, String name, Type type) {
+    }
+
     public enum Type{
         WRAP,PROTEIN,VEGGIES,CHEESE,SAUCE
     }
