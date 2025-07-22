b2679bdf197d66e78d402e6137fb90a0c5b1bbdc
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-07-22 23:37:26 +0500

    Закончил делать форму для дизайна

--- tacoCloud/src/main/java/sia/tacoCloud/data/Ingredient.java
+++ tacoCloud/src/main/java/sia/tacoCloud/data/Ingredient.java
@@ -1,16 +1,17 @@
 package sia.tacoCloud.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
     
     private String id;
     private String name;
     private Type type;
-
-    public Ingredient(String sotName, String name, Type type) {
-    }
 
     public enum Type{
         WRAP,PROTEIN,VEGGIES,CHEESE,SAUCE
